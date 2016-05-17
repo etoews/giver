2ff1af343255f2e5067b28a27822763a75732cb3
Everett Toews <everett.toews@rackspace.com>
2016-05-16 19:20:33 -0500

    Initial commit

---
+++ todo/todo.go
@@ -0,0 +1,52 @@
+package todo
+
+import (
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+)
+
+type Item struct {
+	Text string
+	Priority int
+}
+
+func SaveItems(filename string, items []Item) error {
+	b, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+
+	err = ioutil.WriteFile(filename, b, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadItems(filename string) ([]Item, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return []Item{}, err
+	}
+
+	var items []Item
+	if err := json.Unmarshal(b, &items); err != nil {
+		return []Item{}, err
+	}
+
+	return items, nil
+}
+
+func (i *Item) SetPriority(pri int) {
+	switch pri {
+	case 1:
+		i.Priority = 1
+	case 3:
+		i.Priority = 3
+	default:
+		i.Priority = 2
+	}
+}
